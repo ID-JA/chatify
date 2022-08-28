419b49a089f527619558fd0eae28e45cea7f90ad
achraffawzi <achraffawzi224a@gmail.com>
2022-08-28 22:55:30 +0100

    Install react-query package

--- client/src/App.js
+++ client/src/App.js
@@ -9,25 +9,30 @@ import {
 } from "./pages/auth";
 import Messenger from "./pages/messenger/Messenger.js";
 import { DEFAULT_THEME, MyGlobalStyle } from "./theme";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <MantineProvider withNormalizeCSS withGlobalStyles theme={DEFAULT_THEME}>
-      <MyGlobalStyle />
-      <ColorSchemeProvider colorScheme="dark">
-        <Router>
-          <Routes>
-            <Route path="auth" element={<AuthLayout />}>
-              <Route path="forget-password" element={<ForgetPassword />} />
-              <Route path="signin" element={<SignIn />} />
-              <Route path="signup" element={<SignUp />} />
-              <Route path="reset-password" element={<ResetPassword />} />
-            </Route>
-            <Route path="/messenger" element={<Messenger />} />
-          </Routes>
-        </Router>
-      </ColorSchemeProvider>
-    </MantineProvider>
+    <QueryClientProvider client={queryClient}>
+      <MantineProvider withNormalizeCSS withGlobalStyles theme={DEFAULT_THEME}>
+        <MyGlobalStyle />
+        <ColorSchemeProvider colorScheme="dark">
+          <Router>
+            <Routes>
+              <Route path="auth" element={<AuthLayout />}>
+                <Route path="forget-password" element={<ForgetPassword />} />
+                <Route path="signin" element={<SignIn />} />
+                <Route path="signup" element={<SignUp />} />
+                <Route path="reset-password" element={<ResetPassword />} />
+              </Route>
+              <Route path="/messenger" element={<Messenger />} />
+            </Routes>
+          </Router>
+        </ColorSchemeProvider>
+      </MantineProvider>
+    </QueryClientProvider>
   );
 }
 
